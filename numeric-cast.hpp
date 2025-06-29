f25eef2e6f2d338914f033d905e8d55ffa596584
Alex Sierkov <alex.sierkov@gmail.com>
2025-06-29 13:44:34 +0200

    refine numeric_cast implementation; extend the unit tests; create a benchmark

--- numeric-cast.hpp
+++ numeric-cast.hpp
@@ -7,31 +7,30 @@
 #include "error.hpp"
 
 namespace turbo {
-    template<typename TO, typename FROM>
+    template<std::integral TO, std::integral FROM>
     constexpr TO numeric_cast(const FROM from)
     {
         if constexpr (std::numeric_limits<FROM>::is_signed == std::numeric_limits<TO>::is_signed) {
-          if (from > std::numeric_limits<TO>::max()) [[unlikely]]
-            throw error(fmt::format("can't convert {} {} to {}: the value is larger than {}",
-                typeid(FROM).name(), from, typeid(TO).name(), std::numeric_limits<TO>::max()));
-          if (from < std::numeric_limits<TO>::min()) [[unlikely]]
-              throw error(fmt::format("can't convert {} {} to {}: the value is too small", typeid(FROM).name(), from, typeid(TO).name()));
-          return static_cast<TO>(from);
-        }
-        if constexpr (std::numeric_limits<FROM>::is_signed) {
-            if (from < 0) [[unlikely]]
-                throw error(fmt::format("can't convert {} {} to {}: the value is native", typeid(FROM).name(), from, typeid(TO).name()));
-            if (std::numeric_limits<FROM>::max() > std::numeric_limits<TO>::max()
-                    && from > static_cast<FROM>(std::numeric_limits<TO>::max())) [[unlikely]]
-                throw error(fmt::format("can't convert {} {} to {}: the value is too big", typeid(FROM).name(), from, typeid(TO).name()));
-            return static_cast<TO>(from);
-        }
-        if constexpr (std::numeric_limits<FROM>::digits > std::numeric_limits<TO>::digits) {
-            if (from > static_cast<FROM>(std::numeric_limits<TO>::max())) [[unlikely]]
-                throw error(fmt::format("can't convert {} {} to {}: the value is too big", typeid(FROM).name(), from, typeid(TO).name()));
+            if constexpr (std::numeric_limits<FROM>::max() > std::numeric_limits<TO>::max()) {
+                if (from > static_cast<FROM>(std::numeric_limits<TO>::max())) [[unlikely]]
+                    throw error(fmt::format("can't convert {} {} to {}: the value is larger than {}",
+                        typeid(FROM).name(), from, typeid(TO).name(), std::numeric_limits<TO>::max()));
+                if constexpr (std::numeric_limits<FROM>::is_signed) {
+                    if (from < static_cast<FROM>(std::numeric_limits<TO>::min())) [[unlikely]]
+                        throw error(fmt::format("can't convert {} {} to {}: the value is smaller than {}",
+                            typeid(FROM).name(), from, typeid(TO).name(), std::numeric_limits<TO>::min()));
+                }
+            }
         } else {
-            if (static_cast<TO>(from) > std::numeric_limits<TO>::max()) [[unlikely]]
-                throw error(fmt::format("can't convert {} {} to {}: the value is too big", typeid(FROM).name(), from, typeid(TO).name()));
+            if constexpr (std::numeric_limits<FROM>::is_signed) {
+                if (from < FROM{0}) [[unlikely]]
+                    throw error(fmt::format("can't convert {} {} to {}: the value is negative", typeid(FROM).name(), from, typeid(TO).name()));
+            }
+            if constexpr (std::numeric_limits<FROM>::digits > std::numeric_limits<TO>::digits) {
+                if (from > static_cast<FROM>(std::numeric_limits<TO>::max())) [[unlikely]]
+                    throw error(fmt::format("can't convert {} {} to {}: the value is larger than {}",
+                        typeid(FROM).name(), from, typeid(TO).name(), std::numeric_limits<TO>::max()));
+            }
         }
         return static_cast<TO>(from);
     }
--- numeric-cast.test.cpp
+++ numeric-cast.test.cpp
@@ -10,18 +10,24 @@ namespace {
 
 suite turbo_common_numeric_cast_suite = [] {
     "turbo::common::numeric_cast"_test = [] {
-        expect_equal(uint8_t { 24 }, numeric_cast<uint8_t>(uint64_t { 24 }));
-        expect_equal(int8_t { 24 }, numeric_cast<int8_t>(int64_t { 24 }));
-        expect_equal(int8_t { -24 }, numeric_cast<int8_t>(int64_t { -24 }));
-        expect_equal(uint8_t { 255 }, numeric_cast<uint8_t>(int16_t { 255 }));
-        expect_equal(int64_t { 205665 }, numeric_cast<int64_t>(uint64_t { 205665 }));
-        expect_equal(uint64_t { std::numeric_limits<int64_t>::max() }, numeric_cast<uint64_t>(std::numeric_limits<int64_t>::max()));
+        // both unsigned
+        expect_equal(uint8_t{0}, numeric_cast<uint8_t>(uint64_t{0}));
+        expect_equal(uint8_t{24}, numeric_cast<uint8_t>(uint64_t{24}));
+        expect_equal(uint8_t{255}, numeric_cast<uint8_t>(uint64_t{255}));
         expect(throws([&] { numeric_cast<uint8_t>(256); }));
-        expect(throws([&] { numeric_cast<uint8_t>(256); }));
-        expect(throws([&] { numeric_cast<int8_t>(-129); }));
-        expect(throws([&] { numeric_cast<uint64_t>(int64_t { -250 }); }));
-        expect(throws([&] { numeric_cast<int8_t>(int64_t { -250 }); }));
-        expect(throws([&] { numeric_cast<int8_t>(int64_t { 250 }); }));
-        expect(throws([&] { numeric_cast<int64_t>(std::numeric_limits<uint64_t>::max()); }));
+
+        // both signed
+        expect_equal(int8_t{-128}, numeric_cast<int8_t>(int64_t{-128}));
+        expect_equal(int8_t{24}, numeric_cast<int8_t>(int64_t{24}));
+        expect_equal(int8_t{127}, numeric_cast<int8_t>(int64_t{127}));
+        expect(throws([&] { numeric_cast<int8_t>(int64_t{128}); }));
+        expect(throws([&] { numeric_cast<int8_t>(int64_t{-129}); }));
+
+        // differing signedness
+        expect(throws([&] { numeric_cast<uint64_t>(int64_t{-1}); }));
+        expect_equal(uint8_t{0}, numeric_cast<uint8_t>(int8_t{0}));
+        expect_equal(int8_t{0}, numeric_cast<int8_t>(uint8_t{0}));
+        expect_equal(uint8_t{255}, numeric_cast<uint8_t>(int64_t{255}));
+        expect(throws([&] { numeric_cast<uint8_t>(int64_t{256}); }));
     };
 };
