c754cf60f93f8430a23685f056ff416f781af778
Alex Sierkov <alex.sierkov@gmail.com>
2025-07-18 20:07:36 +0200

    fix a compilation error with Clang 19 on Linux

--- bytes.test.cpp
+++ bytes.test.cpp
@@ -1,6 +1,7 @@
 /* Copyright (c) 2022-2023 Alex Sierkov (alex dot sierkov at gmail dot com)
  * Copyright (c) 2024-2025 R2 Rationality OÜ (info at r2rationality dot com) */
 
+#include <numeric>
 #include "bytes.hpp"
 #include "test.hpp"
 
