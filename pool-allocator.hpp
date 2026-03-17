3f299ecce574c3f6b10d674bc4b9988bb4d4bf33
Alex Sierkov <alex.sierkov@gmail.com>
2026-03-17 07:46:25 +0200

    minor refinements to pool-allocator after a code review

--- pool-allocator.hpp
+++ pool-allocator.hpp
@@ -1,17 +1,23 @@
 #pragma once
 /* Copyright (c) 2022-2023 Alex Sierkov (alex dot sierkov at gmail dot com)
- * Copyright (c) 2024-2025 R2 Rationality OÜ (info at r2rationality dot com) */
+ * Copyright (c) 2024-2026 R2 Rationality OÜ (info at r2rationality dot com) */
 
 #include <forward_list>
 #include <memory>
 #include <vector>
 
 namespace turbo {
-    template<typename T>
-    struct pool_deleter_t;
-
-    template<typename T, size_t BATCH_SZ = 0x1000>
+    template<typename T, size_t BATCH_SZ = 0x1000, bool SKIP_DTOR = std::is_trivially_destructible_v<T>>
     struct pool_allocator_t {
+        static_assert(SKIP_DTOR, "pool_allocator_t does not call T's destructor on pool destruction; "
+            "set SKIP_DTOR=true to acknowledge this if T only owns resources within the same pool");
+
+        pool_allocator_t() = default;
+        pool_allocator_t(const pool_allocator_t &) = delete;
+        pool_allocator_t(pool_allocator_t &&) = delete;
+        pool_allocator_t &operator=(const pool_allocator_t &) = delete;
+        pool_allocator_t &operator=(pool_allocator_t &&) = delete;
+
         struct deleter_t {
             pool_allocator_t *_alloc = nullptr;
 
@@ -46,15 +52,24 @@ namespace turbo {
         ~pool_allocator_t()
         {
             for (auto a: _arenas)
-                operator delete(a);
+                operator delete(a, std::align_val_t{alignof(T)});
         }
 
         template<typename... Args>
         ptr_t make_ptr(Args&&... args)
         {
             T* raw = allocate();
-            new (raw) T { std::forward<Args>(args)... };  // Placement new
+            try {
+                new (raw) T { std::forward<Args>(args)... };
+            } catch (...) {
+                deallocate(raw);
+                throw;
+            }
             return { raw, _deleter };
+        }
+
+        size_t free_count() const {
+            return std::distance(_free.begin(), _free.end());
         }
     private:
         std::vector<void *> _arenas {};
@@ -64,7 +79,7 @@ namespace turbo {
 
         void _add_arena()
         {
-            _arenas.push_back(operator new (BATCH_SZ * sizeof(T)));
+            _arenas.push_back(operator new(BATCH_SZ * sizeof(T), std::align_val_t{alignof(T)}));
             _arena_offset = 0;
         }
     };
