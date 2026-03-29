781b1d3ea09fcecf3cbd5aec39b4c60a67de23e0
Alex Sierkov <alex.sierkov@gmail.com>
2026-03-29 20:32:03 +0300

    pool-allocator now supports optional zeroing

--- pool-allocator.hpp
+++ pool-allocator.hpp
@@ -2,15 +2,43 @@
 /* Copyright (c) 2022-2023 Alex Sierkov (alex dot sierkov at gmail dot com)
  * Copyright (c) 2024-2026 R2 Rationality OÜ (info at r2rationality dot com) */
 
+#include <cstdint>
+#include <cstring>
 #include <forward_list>
 #include <memory>
 #include <vector>
 
 namespace turbo {
-    template<typename T, size_t BATCH_SZ = 0x1000, bool SKIP_DTOR = std::is_trivially_destructible_v<T>>
+    enum class zero_policy_t: uint8_t {
+        none = 0,
+        new_arena = 1,
+        free_list = 2,
+        all = new_arena | free_list
+    };
+
+    template<typename T, zero_policy_t ZERO_POLICY>
+    struct zero_policy_type_ok_t: std::true_type {};
+
+    template<typename T>
+    struct zero_policy_type_ok_t<T, zero_policy_t::new_arena>: std::bool_constant<std::is_trivially_copyable_v<T>> {};
+
+    template<typename T>
+    struct zero_policy_type_ok_t<T, zero_policy_t::free_list>: std::bool_constant<std::is_trivially_copyable_v<T>> {};
+
+    template<typename T>
+    struct zero_policy_type_ok_t<T, zero_policy_t::all>: std::bool_constant<std::is_trivially_copyable_v<T>> {};
+
+    [[nodiscard]] constexpr bool zero_policy_has(const zero_policy_t policy, const zero_policy_t flag) noexcept
+    {
+        return (static_cast<uint8_t>(policy) & static_cast<uint8_t>(flag)) != 0;
+    }
+
+    template<typename T, size_t BATCH_SZ = 0x1000, bool SKIP_DTOR = std::is_trivially_destructible_v<T>, zero_policy_t ZERO_POLICY = zero_policy_t::none>
     struct pool_allocator_t {
         static_assert(SKIP_DTOR, "pool_allocator_t does not call T's destructor on pool destruction; "
             "set SKIP_DTOR=true to acknowledge this if T only owns resources within the same pool");
+        static_assert(zero_policy_type_ok_t<T, ZERO_POLICY>::value,
+            "pool_allocator_t zeroing is only supported for trivially copyable T");
 
         pool_allocator_t() = default;
         pool_allocator_t(const pool_allocator_t &) = delete;
@@ -45,8 +73,11 @@ namespace turbo {
 
         void deallocate(T* ptr)
         {
-            if (ptr)
+            if (ptr) {
+                if constexpr (zero_policy_has(ZERO_POLICY, zero_policy_t::free_list))
+                    std::memset(ptr, 0, sizeof(T));
                 _free.push_front(ptr);
+            }
         }
 
         ~pool_allocator_t()
@@ -79,7 +110,10 @@ namespace turbo {
 
         void _add_arena()
         {
-            _arenas.push_back(operator new(BATCH_SZ * sizeof(T), std::align_val_t{alignof(T)}));
+            auto *arena = operator new(BATCH_SZ * sizeof(T), std::align_val_t{alignof(T)});
+            if constexpr (zero_policy_has(ZERO_POLICY, zero_policy_t::new_arena))
+                std::memset(arena, 0, BATCH_SZ * sizeof(T));
+            _arenas.push_back(arena);
             _arena_offset = 0;
         }
     };
